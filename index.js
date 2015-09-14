840e8eb59ba49b708a1f222fbf90b6f21c8c88a3
Kevin Barabash <kevinb@khanacademy.org>
2015-09-13 22:03:48 -0700

    added functions to create and draw runs, added text box input to modify lhs and rhs

--- index.js
+++ index.js
@@ -16,16 +16,97 @@ document.body.appendChild(canvas);
 var ctx = canvas.getContext('2d');
 
 ctx.scale(ratio, ratio);
-ctx.font = '60px XitsMath';
 
-var msg = "Hello, world";
-ctx.fillText(msg, 100, 100);
+ctx.font = '100 84px Helvetica Neue';
+
+let math = "x + 1 = 5";
+let [left, right] = math.split("=");
+
+let space = ctx.measureText(" ").width;
+
+let createRun = function(math) {
+    let regex = /[a-z]|[0-9]|[\+\-\=]|[\u0374-\u03FF]/g;
+    let m, x = 0, y = 0;
+    let children = [];
+    let anchor = { x:0, y:0 };
+    while (m = regex.exec(math)) {
+        let codePoint = m[0].charCodeAt(0);
+
+        if (/[a-z]/.test(m[0])) {
+            //codePoint = m[0].charCodeAt(0) - 97 + 0x1D44E;
+        }
+        if (m[0] === "-") {
+            codePoint = 0x2212;
+        }
+        if (/[\+\-=]/.test(m[0])) {
+            x += space;
+        }
+        let glyph = String.fromCodePoint(codePoint);
+        let width = ctx.measureText(glyph).width;
+
+        children.push({ glyph, x, y });
+        if (m[0] === "=") {
+            anchor.x = x + width / 2;
+        }
+
+        x += width;
+        if (/[\+\-=]/.test(m[0])) {
+            x += space;
+        }
+    }
+
+    return {
+        children, anchor
+    };
+};
+
+let drawRun = function(run, x, y) {
+    for (let glyph of run.children) {
+        ctx.fillText(
+            glyph.glyph,
+            glyph.x + x - run.anchor.x,
+            glyph.y + y - run.anchor.y
+        );
+    }
+};
 
 
-msg = "\u{1D44E}\u{1D44F}" + String.fromCodePoint(0x1D450);
-ctx.fillText(msg, 100, 200);
-console.log(msg.charCodeAt(0).toString(16));
+let drawAxes = function() {
+    ctx.strokeStyle = 'red';
+    ctx.beginPath();
+    ctx.moveTo(400, 0);
+    ctx.lineTo(400, 600);
+    ctx.moveTo(0, 300);
+    ctx.lineTo(800, 300);
+    ctx.stroke();
+};
 
 
+let run = createRun(math);
 
+let draw = function() {
+    ctx.clearRect(0, 0, 800, 600);
+    run = createRun(math);
+    drawRun(run, 800 / 2, 600 / 2 + 21);
+    //drawAxes();
+    //requestAnimationFrame(draw);
+};
 
+draw();
+
+let updateButton = document.getElementById('updateButton');
+let updateText = document.getElementById('updateText');
+
+updateButton.addEventListener('click', function() {
+    console.log(updateText.value);
+    ctx.clearRect(0, 0, 800, 600);
+
+    left += updateText.value;
+    right += updateText.value;
+
+    math = [left, right].join("=");
+
+    run = createRun(math);
+
+    draw();
+});
